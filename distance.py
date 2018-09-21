08f53572854a1740e5ef307779f8721969617842
bananafan <43342160+bananafan@users.noreply.github.com>
2018-09-21 10:18:32 +0100

    add test for computing distance

---
+++ distance.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+File for computing the minimum distance between points
+"""
+import math
+
+def compute_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1]) **2)
+
+point1 = (0,0)
+point2 = (1,1)
+print(compute_distance(point1,point2))
